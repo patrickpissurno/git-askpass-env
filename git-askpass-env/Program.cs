7aceceea81358c93430560d27cf7d11614617349
Patrick Pissurno <contato@patrickpissurno.com.br>
2020-05-11 16:15:41 -0300

    v1.0.0

---
+++ git-askpass-env/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace git_askpass_env
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var password = Environment.GetEnvironmentVariable("GIT_PASSWORD");
+
+            if (string.IsNullOrEmpty(password))
+                Environment.Exit(1);
+
+            Console.Write(password);
+        }
+    }
+}
---
+++ git-askpass-env/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("git-askpass-env")]
+[assembly: AssemblyDescription("Replace git-askpass.exe with this one to make git get your credentials from environment variables")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Patrick Pissurno")]
+[assembly: AssemblyProduct("git-askpass-env")]
+[assembly: AssemblyCopyright("Copyright © Patrick Pissurno 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5150ad71-d160-4d8b-b46a-204ad80cb53f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
